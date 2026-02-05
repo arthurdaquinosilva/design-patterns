fde1a878eebb8be4ab9120fb61bcc1e1dc98a3b2
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-02-05 13:47:17 -0300

    refactor(state-pattern): final implementation of state pattern

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-import memento.Editor;
-import memento.History;
+import state.BrushTool;
+import state.Canvas;
 
 public class Main {
     public static void main(String[] args) {
-        var editor = new Editor();
-        var history = new History();
-
-        editor.setContent("Content 1");
-        history.push(editor.createState());
-
-        editor.setContent("Content 2");
-        history.push(editor.createState());
-
-        editor.setContent("Content 3");
-        history.push(editor.createState());
-
-        editor.setContent("Content 4");
-        editor.restore(history.pop());
-
-        System.out.println(editor.getContent());
+        var canvas = new Canvas();
+        canvas.setCurrentTool(new BrushTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
     }
 }
---
+++ state/EraseTool.java
@@ -0,0 +1,14 @@
+package state;
+
+public class EraseTool implements Tool {
+    @Override
+    public void mouseDown() {
+        System.out.println("Erase icon");
+    }
+
+    @Override
+    public void mouseUp() {
+        System.out.println("Erase something");
+    }
+
+}
